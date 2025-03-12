4b12db995633e78fe55189a1c3bf8249edc34994
loisina <adjoaasiedu4@gmail.com>
2025-03-12 13:46:43 +0000

    I have added the multer middle ware and the cors package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import e from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import LibraryRouter from "./routes/library-routes.js";
 
 
@@ -7,10 +8,13 @@ await mongoose.connect (process.env.MONGO_URI)
 
 const app = e();
 app.use (e.json())
+app.use (cors())
 
 // use router here
 app.use(LibraryRouter);
 
-app.listen(4000, () =>{
-  console.log("Server is running on port 4000")
+// Listen for incoming request
+const port = process.env.PORT || 4000;
+app.listen(port, ()=>{
+  console.log(`Server listening on port ${port}`)
 })
--- routes/library-routes.js
+++ routes/library-routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { addBook, deleteBook, getAllBooks, getBookById, updateBook } from "../controllers/library-controller.js";
+import { remoteUpload } from "../middlewares/upload.js";
 
 const LibraryRouter = Router();
 
-LibraryRouter.post('/book', addBook);
+LibraryRouter.post('/book',remoteUpload.single('image'), addBook);
 LibraryRouter.get('/book', getAllBooks);
 LibraryRouter.get('/book/:id', getBookById);
 LibraryRouter.patch('/book/:id', updateBook);
